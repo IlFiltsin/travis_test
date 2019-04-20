12b7c79959d8a0cdb25c2dd22c7b296a1a92da21
Ilya Filtsin <ilfiltsin@gmail.com>
2019-04-20 13:30:29 +0300

    Update travis-build

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,5 +1,5 @@
 echo "Compiling unit tests..."
-g++ -std=c++11 -o unit_test -lgtest -lgtest_main main.cpp -Wall -Wextra -pedantic -pedantic-errors -v
+g++ -std=c++11 -o unit_test -lgtest -lgtest_main -D_ELPP_DEFAULT_LOG_FILE='"logs/el.gtest.log"' -DELPP_LOGGING_FLAGS_FROM_ARG -DELPP_FEATURE_ALL main.cpp -Wall -Wextra -pedantic -pedantic-errors -v
 echo "Running unit tests..."
 ./unit_test -v
 result=$?
