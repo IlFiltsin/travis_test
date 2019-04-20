9e17bf5a491faffab89de565e9aae96b7c0211fc
Ilya Filtsin <ilfiltsin@gmail.com>
2019-04-20 12:52:46 +0300

    Update travis

--- .travis-build.sh
+++ .travis-build.sh
@@ -1,5 +1,5 @@
 echo "Compiling unit tests..."
-g++ -std=c++11 -o unit_test -lgtest main.cpp -Wall -Wextra -pedantic -pedantic-errors
+g++ -std=c++11 -o unit_test -lgtest -lgtest_main main.cpp -Wall -Wextra -pedantic -pedantic-errors
 echo "Running unit tests..."
 ./unit_test
 result=$?
